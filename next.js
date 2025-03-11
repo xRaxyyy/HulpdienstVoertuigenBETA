e1490648eb3d6e13d4ca598a225ef761dc165904
xRaxyyy <xraxyyy@gmail.com>
2025-03-11 10:28:31 +0100

    Initial commit

--- next.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Environment Variable Test</h1>
-      <p id="output">Value from env: {process.env.NEXT_PUBLIC_TEST_VAR}</p>
-    </div>
-  );
-}
